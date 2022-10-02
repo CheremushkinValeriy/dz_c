8492354d93aa5443ba0bf1acd7b5723c9d42288e
Valeriy <cheremushkin.valeriy@mail.ru>
2022-10-03 00:23:43 +0400

    Решение второй задачи

--- zd_2/Program.cs
+++ zd_2/Program.cs
@@ -1,8 +1,20 @@
-﻿int numberA = new Random().Next (3,10);
-Console.WriteLine(numberA);
-int numberB = new Random().Next (3,10);
-Console.WriteLine(numberB);
-int result = numberA + numberB;
-Console.WriteLine(result);
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+string userInput_1 = Console.ReadLine() ??"";
+double number1 = double.Parse(userInput_1);
 
+Console.Write("Введите второе число: ");
+string userInput_2 = Console.ReadLine() ??"";
+double number2 = double.Parse(userInput_2);
+
+if (number1 > number2)
+{
+    Console.WriteLine("max = "+ number1);
+    Console.WriteLine("min = "+ number2);
+}
+else
+{
+    Console.WriteLine("max = "+ number2);
+    Console.WriteLine("min = "+ number1);
+}
 
