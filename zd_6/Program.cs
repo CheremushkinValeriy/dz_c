1f32eb940e9ec8354eed12e42650f712228d61ca
Valeriy <cheremushkin.valeriy@mail.ru>
2022-10-03 20:27:13 +0400

    Решение шестой задачи

---
+++ zd_6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+string userInput_1 = Console.ReadLine() ??"";
+double number1 = double.Parse(userInput_1);
+ 
+if (number1 %2 == 0)
+{
+    Console.WriteLine("Число является четным");
+}
+else
+{
+    Console.WriteLine("Число является нечетным");
+}
