f91a74c2a5f2f78f59d192423c44aed21c3eb13f
Valeriy <cheremushkin.valeriy@mail.ru>
2022-10-03 21:26:41 +0400

    Решение восьмой задачи

---
+++ zd_8/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+string userInput = Console.ReadLine() ?? "";
+int number = int.Parse(userInput);
+
+int i = 0;
+
+while (i < number)
+{
+    if (i % 2 == 0)
+    {
+        i += 1;
+    }
+    else
+    {
+        i += 1;
+        Console.Write(i +",");
+    }
+}
+
+
+
