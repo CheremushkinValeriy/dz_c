2cfbc29ca4c8f3d8cf18e26babb72e600eb4bc72
Valeriy <cheremushkin.valeriy@mail.ru>
2022-10-06 20:40:17 +0400

    Решение десятой задачи

---
+++ dz_10/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+
+while(true)
+{
+    Console.Write("Введите число: ");
+    string userInput = Console.ReadLine() ??"";
+    int number = int.Parse(userInput);
+
+    if (number >= 100 & number < 1000) 
+        {
+            Console.WriteLine($"Число = {number}");
+            int a1 = number / 10;
+            int a2 = a1 % 10;
+            Console.WriteLine($"Вротая цифра в числе {number} -> {a2}");
+            break;
+        }
+    else
+    {
+        Console.Write("Введено не корректное число --> ");
+    }
+
+
+
+}
+
+    
+
+ 
