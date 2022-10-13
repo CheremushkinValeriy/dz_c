00babcf581c09b8ffa25d08575f7707a788319da
Valeriy <cheremushkin.valeriy@mail.ru>
2022-10-13 21:21:12 +0400

    Решение двадцать девятой задачи

--- dz_29/Program.cs
+++ dz_29/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+ 
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+ 
+
+Console.Write("Введите размер массива: ");
+string userInput = Console.ReadLine() ??"";
+int size = int.Parse(userInput);
+//--------------------------------------------
+int[] Arrey = InputRandom(size);
+Console.Write("[");
+for (int i = 0; i < size; i++)
+{
+  Console.Write(Arrey[i] + ",");
+} 
+Console.Write("]");
+//---------------------------------------------
+int[] InputRandom (int size)
+{
+  int[] arrey = new int[size];
+  for (int i = 0; i < size; i++)
+  {
+    arrey[i] = new Random().Next(0, 100);
+    //можно добавить еще ввод начал/конц рандомного выбора чисел и три переменной передавать методу.
+  }
+  return arrey;
+} 
