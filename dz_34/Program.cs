db263e3b5077e598746aa5976095c7f7836c72a5
Valeriy <cheremushkin.valeriy@mail.ru>
2022-10-19 20:24:55 +0400

    Решение 34 задачи

---
+++ dz_34/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+ 
+// [345, 897, 568, 234] -> 2
+  
+int[] array = GetArray (4, 100, 999);
+int EvenNumbers = GetEvenNumbers(array);
+PrintEvenNumbers(array);
+Console.WriteLine("-> " + EvenNumbers);
+//------------------------------------------------
+int[] GetArray (int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+        res[i] = new Random().Next(minValue, maxValue +1);
+    return res;
+}
+//-----------------------------------------------------
+int GetEvenNumbers(int[] array)
+{
+    int EvenNumbers = 0;
+    foreach(int element in array)
+    {
+        if (element % 2 == 0)  
+        EvenNumbers ++;
+    }
+    return EvenNumbers;
+}
+//-----------------------------------------------------
+void PrintEvenNumbers(int[] arrey)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+}
